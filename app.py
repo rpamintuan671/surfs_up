8a7357a9b63d544245d325e1c624cec199d98a38
rpamintuan671 <106283411+rpamintuan671@users.noreply.github.com>
2022-08-30 15:54:54 -0700

    Appy.py updated

--- app.py
+++ app.py
@@ -1,6 +1,42 @@
-from flask import Flask
-app = Flask(__name__)
-@app.route('/')
-def hello_world():
-    return 'Hello world'
 
+import datetime as dt
+import numpy as np
+import pandas as pd
+
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+from flask import Flask, jsonify
+app = Flask(__name__)
+
+
+engine = create_engine("sqlite:///hawaii.sqlite")
+Base = automap_base()
+Base.prepare(engine, reflect=True)
+Measurement = Base.classes.measurement
+Station = Base.classes.station
+session = Session(engine)
+
+
+
+
+@app.route("/api/v1.0/temp/<start>/<end>")
+def stats(start=None, end=None):
+    sel = [func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)]
+
+    if not end:
+        results = session.query(*sel).\
+            filter(Measurement.date >= start).all()
+        temps = list(np.ravel(results))
+        return jsonify(temps)
+
+    results = session.query(*sel).\
+        filter(Measurement.date >= start).\
+        filter(Measurement.date <= end).all()
+    temps = list(np.ravel(results))
+    return jsonify(temps)
+
+if __name__ == '__main__':
+   app.run()
